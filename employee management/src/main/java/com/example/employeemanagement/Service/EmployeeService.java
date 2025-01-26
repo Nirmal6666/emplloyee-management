defb58b9c47bbd22939db2061ed06a698ddc9056
Anjana <aaluwihare4@gmail.com>
2025-01-26 21:13:04 +0530

    pt1

---
+++ employee management/src/main/java/com/example/employeemanagement/Service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.example.employeemanagement.Service;
+
+import com.example.employeemanagement.DTO.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
